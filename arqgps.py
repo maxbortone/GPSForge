9aeae53ae6cb504a63ac19ded7d2f366c6fb9f33
Massimo Bortone <maxbortone@gmail.com>
2021-07-14 22:56:50 +0100

    Clean up code

--- arqgps.py
+++ arqgps.py
@@ -1,22 +1,21 @@
-import flax.linen as nn
+from typing import Tuple
 import jax.numpy as jnp
 from jax import vmap
 from jax.scipy.special import logsumexp
+from netket.hilbert import Spin
 from netket.utils.types import DType, Array, NNInitFunc, PyTree
 from netket.models import ARNN
 from initializers import gaussian
 
 
+def local_states_to_indices(hilbert : Spin, states : Array) -> Array:
+    indices = (states+hilbert.local_size-1)/2
+    indices = jnp.asarray(indices, jnp.int32)
+    return indices
+
 def l2_normalize(log_psi : Array) -> Array:
     return log_psi - 0.5*logsumexp(2*log_psi.real, axis=-1, keepdims=True)
 
-def compute_conditional_log_psi(eps : Array, conf : Array) -> Array:
-    idx = jnp.expand_dims(nn.relu(conf), axis=(0,1))
-    vals = jnp.take_along_axis(eps, idx, axis=0)
-    vals = jnp.pad(vals[:,:,:-1], ((0,0),(0,0),(1,0)), constant_values=1.0)
-    vals = jnp.cumprod(vals, axis=-1)
-    log_conds = jnp.sum(eps*vals, axis=1).T
-    return log_conds # (L, 2)
 
 class ARQGPS(ARNN):
 
@@ -25,23 +24,35 @@ class ARQGPS(ARNN):
     dtype: DType = jnp.float64
     eps_init: NNInitFunc = gaussian()
 
-    def conditionals(self, x_in: Array, cache: PyTree):
-        log_psi = vmap(compute_conditional_log_psi, in_axes=(None, 0))(self.eps, x_in)
-        log_psi = l2_normalize(log_psi)
+    def conditionals(self, inputs: Array, cache: PyTree) -> Tuple[Array, PyTree]:
+        log_psi = _conditionals(self, inputs)
         p = jnp.exp(2*log_psi.real)
-        return p, cache
+        return p, cache # (B, L, 2)
 
     def setup(self):
         self.eps = self.param("epsilon", self.eps_init, (2, self.N, self.L), self.dtype)
 
-    def __call__(self, x_in : Array):
-        if jnp.ndim(x_in) == 1:
-            x_in = jnp.expand_dims(x_in, axis=0)
-        idx = nn.relu(x_in) # transforms [-1,+1]->[0,+1]
-        idx = jnp.asarray(idx, dtype=jnp.int64)
+    def __call__(self, inputs: Array):
+        if jnp.ndim(inputs) == 1:
+            inputs = jnp.expand_dims(inputs, axis=0)
+        idx = local_states_to_indices(self.hilbert, inputs)
         idx = jnp.expand_dims(idx, axis=-1)
-        log_psi = vmap(compute_conditional_log_psi, in_axes=(None, 0))(self.eps, x_in)
-        log_psi = l2_normalize(log_psi)
+        log_psi = _conditionals(self, inputs)
         log_psi = jnp.take_along_axis(log_psi, idx, axis=-1)
-        log_psi = jnp.sum(jnp.reshape(log_psi, (x_in.shape[0], -1)), axis=1)
+        log_psi = jnp.sum(jnp.reshape(log_psi, (inputs.shape[0], -1)), axis=1)
         return log_psi
+
+
+def _conditionals(model, inputs):
+
+    def _compute_conditional_log_psi(eps, input):
+        idx = jnp.expand_dims(local_states_to_indices(model.hilbert, input), axis=(0,1))
+        vals = jnp.take_along_axis(eps, idx, axis=0)
+        vals = jnp.pad(vals[:,:,:-1], ((0,0),(0,0),(1,0)), constant_values=1.0)
+        vals = jnp.cumprod(vals, axis=-1)
+        log_conds = jnp.sum(eps*vals, axis=1).T
+        return log_conds # (L, 2)
+    
+    log_psi = vmap(_compute_conditional_log_psi, in_axes=(None, 0))(model.eps, inputs)
+    log_psi = l2_normalize(log_psi)
+    return log_psi # (B, L, 2)
--- qgps.py
+++ qgps.py
@@ -5,13 +5,6 @@ from netket.utils.types import DType, Array, NNInitFunc
 from initializers import gaussian
 
 
-def compute_log_psi(eps, conf):
-    idx = jnp.expand_dims(nn.relu(conf), axis=(0,1))
-    vals = jnp.take_along_axis(eps, idx, axis=0)
-    prods = jnp.prod(vals, axis=-1)
-    log_psi = jnp.sum(prods)
-    return log_psi
-
 class QGPS(nn.Module):
 
     N: jnp.integer = 1
@@ -19,10 +12,18 @@ class QGPS(nn.Module):
     eps_init: NNInitFunc = gaussian()
 
     @nn.compact
-    def __call__(self, x_in: Array):
-        L = x_in.shape[-1]
-        if jnp.ndim(x_in) == 1:
-            x_in = jnp.expand_dims(x_in, axis=0)
+    def __call__(self, inputs: Array):
+        L = inputs.shape[-1]
+        if jnp.ndim(inputs) == 1:
+            inputs = jnp.expand_dims(inputs, axis=0)
         eps = self.param("epsilon", self.eps_init, (2, self.N, L), self.dtype)
-        log_psi = vmap(compute_log_psi, in_axes=(None, 0))(eps, x_in)
+
+        def _compute_log_psi(eps, conf):
+            idx = jnp.expand_dims(nn.relu(conf), axis=(0,1))
+            vals = jnp.take_along_axis(eps, idx, axis=0)
+            prods = jnp.prod(vals, axis=-1)
+            log_psi = jnp.sum(prods)
+            return log_psi
+        
+        log_psi = vmap(_compute_log_psi, in_axes=(None, 0))(eps, inputs)
         return log_psi
