afcdc685b2a67651213b9be13e6301f58c195e33
Massimo Bortone <maxbortone@gmail.com>
2021-09-28 13:10:41 +0200

    Add model key to config and improve result plot

--- plot_result.py
+++ plot_result.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from utils import dir_path, read_config, unpack_result
+from pprint import pprint
 
 
 # Parse arguments
@@ -22,6 +23,7 @@ plt.style.use('seaborn-dark')
 
 # Get config
 config = read_config(args.path)
+pprint(vars(config))
 
 # Extract energies and errors
 data = unpack_result(args.path)
@@ -30,8 +32,16 @@ sigma = data['sigma']
 iters = data['iters']
 
 # Get exact energy
-df = pd.read_csv('result_DMRG_Heisenberg_1D.csv', dtype={'L': np.int64, 'E': np.float64})
-exact_energy = 4*df.loc[df['L']==config.L]['E'].values[0]
+base_path = os.path.dirname(os.path.abspath(__file__))
+if config.model == "heisenberg1d":
+    path = os.path.join(base_path, 'result_DMRG_Heisenberg_1D.csv')
+    df = pd.read_csv(path, dtype={'L': np.int64, 'E': np.float64})
+    exact_energy = 4*df.loc[df['L']==config.L]['E'].values[0]
+elif config.model == "j1j22d":
+    path = os.path.join(base_path, 'result_ED_J1J2_2D.csv')
+    df = pd.read_csv(path, dtype={'L': np.int64, 'J1': np.float32, 'J2': np.float32, 'E/L^2': np.float32, 'E': np.float32})
+    exact_energy = 4*df.loc[(df['L']==config.L) & (df['J1']==config.J1) & (df['J2']==config.J2)]['E'].values[0]
+
 
 # Compute relative error
 rel_error = np.abs((energy-exact_energy)/exact_energy)
@@ -50,6 +60,8 @@ ax.axhline(exact_energy, linestyle='dashed', color='k')
 ax.grid(True)
 ax.set_xlabel('Iteration')
 ax.set_ylabel('Energy')
+if args.title:
+    ax.set_title(args.title)
 if args.save:
     plt.savefig(os.path.join(args.path, "energy.png"))
 
@@ -59,6 +71,8 @@ ax.set_yscale('log')
 ax.grid(True)
 ax.set_xlabel('Iteration')
 ax.set_ylabel('Rel. error')
+if args.title:
+    ax.set_title(args.title)
 if args.save:
     plt.savefig(os.path.join(args.path, "rel_error.png"))
 plt.show()
