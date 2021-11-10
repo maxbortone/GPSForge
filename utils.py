49623dfefab7ac98cf88cabe7a67a6cf6ff92f8f
Massimo Bortone <maxbortone@gmail.com>
2021-11-10 17:27:01 +0000

    Implement function to import results from literature

--- utils.py
+++ utils.py
@@ -111,17 +111,35 @@ def list_results(paths):
     df = df.reset_index(drop=True)
     return df
 
+def get_literature_energy(model, ansatz, **filters):
+    df = None
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    if model == "heisenberg1d":
+        pass
+    elif model == "j1j22d":
+        path = os.path.join(base_path, f"result_{ansatz.upper()}_J1J2_2D.csv")
+        df = pd.read_csv(path, dtype={'L': np.int16, 'N': np.int16, 'symm': str, 'J1': np.float32, 'J2': np.float32, 'E_real': np.float32, 'E_imag': np.float32, 'error': np.float32})
+        # Filter rows
+        for key, value in filters.items():
+            if isinstance(value, int) or isinstance(value, float) or isinstance(value, str):
+                df = df.loc[df[key] == value]
+            elif isinstance(value, list):
+                df = df.loc[df[key].isin(value)]
+            df['ansatz'] = ansatz
+            df = df.reset_index(drop=True)
+    return df
+
 def get_exact_energy(model, config):
     exact_energy = None
     base_path = os.path.dirname(os.path.abspath(__file__))
     if model == "heisenberg1d":
         path = os.path.join(base_path, 'result_DMRG_Heisenberg_1D.csv')
-        df = pd.read_csv(path, dtype={'L': np.int64, 'E': np.float64})
+        df = pd.read_csv(path, dtype={'L': np.int16, 'E': np.float64})
         if (df['L']==config.L).any():
             exact_energy = 4*df.loc[df['L']==config.L]['E'].values[0]
     elif model == "j1j22d":
         path = os.path.join(base_path, 'result_ED_J1J2_2D.csv')
-        df = pd.read_csv(path, dtype={'L': np.int64, 'J1': np.float32, 'J2': np.float32, 'E/L^2': np.float32, 'E': np.float32})
+        df = pd.read_csv(path, dtype={'L': np.int16, 'J1': np.float32, 'J2': np.float32, 'E/L^2': np.float32, 'E': np.float32})
         if ((df['L']==config.L) & (df['J1']==config.J1) & (df['J2']==config.J2)).any():
             exact_energy = df.loc[(df['L']==config.L) & (df['J1']==config.J1) & (df['J2']==config.J2)]['E'].values[0]
     return exact_energy
