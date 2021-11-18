f6c2aa7c6af370f8236956ae10f81053c700ef2f
Massimo Bortone <maxbortone@gmail.com>
2021-11-18 17:15:57 +0000

    Make J2 argument optional

--- plot_j1j22d_comparison.py
+++ plot_j1j22d_comparison.py
@@ -14,7 +14,7 @@ parser.add_argument('--L', type=int, required=True,
     help='Number of sites in the system')
 parser.add_argument('--N', type=parse_range, required=True,
     help='Range of bond dimensions')
-parser.add_argument('--J2', type=float, required=True,
+parser.add_argument('--J2', type=float,
     help='Next-nearest neighbor coupling')
 parser.add_argument('--paths', type=dir_path, nargs='+', required=True,
     help='Paths to results')
@@ -33,12 +33,11 @@ sns.set_theme(style='darkgrid')
 df = list_results(args.paths)
 df = df.loc[df['L'] == args.L]
 df = df.loc[df['N'].isin(args.N)]
-df = df.loc[df['J2'] == args.J2]
+if isinstance(args.J2, float):
+    df = df.loc[df['J2'] == args.J2]
+else:
+    args.J2 = list(np.unique(df['J2'].values))
 df = df.reset_index(drop=True)
-
-# Get exact energy
-config = read_config(df['path'][0])
-exact_energy = get_exact_energy('j1j22d', config)
 
 # Extract energy estimates
 num_rows = len(df.index)
@@ -46,6 +45,8 @@ energies = np.zeros(num_rows)
 errors = np.zeros(num_rows)
 rel_errors = np.zeros(num_rows)
 for index, row in df.iterrows():
+    config = read_config(row['path'])
+    exact_energy = get_exact_energy('j1j22d', config)
     data = unpack_result(row['path'])
     energy = data['energy'][-1].real
     error = data['sigma'][-1]
@@ -64,6 +65,8 @@ if args.compare:
     num_rows = len(df_qgps.index)
     rel_errors = np.zeros(num_rows)
     for index, row in df_qgps.iterrows():
+        config = argparse.Namespace(L=args.L, J1=row['J1'], J2=row['J2'])
+        exact_energy = get_exact_energy('j1j22d', config)
         energy = row['energy']
         rel_error = np.abs(energy-exact_energy)/np.abs(exact_energy)
         rel_errors[index]= rel_error
@@ -74,21 +77,37 @@ if args.compare:
     df = df.reset_index(drop=True)
 
 # Print table
-df = df.sort_values(by=['N', 'ansatz', 'samples'], ascending=[True, True, True])
-print(df[['N', 'ansatz', 'samples', 'energy', 'error', 'rel_error', 'uuid']].to_markdown(index=False, tablefmt='github'))
+df = df.sort_values(by=['N', 'ansatz', 'J2', 'samples'], ascending=[True, True, True, True])
+print(df[['N', 'ansatz', 'J2', 'samples', 'energy', 'error', 'rel_error', 'uuid']].to_markdown(index=False, tablefmt='github'))
 
 # Plot
-f = sns.relplot(
-    data=df, kind='line',
-    x='N', y='rel_error',
-    hue='samples', style='ansatz',
-    markers=True, dashes=False,
-    palette='tab10', markersize=10)
-f.set(xscale='log', yscale='log')
-f.tight_layout()
-f.axes[0,0].set_ylabel(r"$|E_{\theta}-E_{gs}|/|E_{gs}|$")
-f.axes[0,0].set_xticks(np.unique(df['N']))
-f.axes[0,0].set_xticklabels(np.unique(df['N']))
+unique_N = np.unique(df['N'].values)
+if isinstance(args.J2, float):
+    f = sns.relplot(
+        data=df, kind='line',
+        x='N', y='rel_error',
+        hue='samples', style='ansatz',
+        markers=True, dashes=False,
+        palette='tab10', markersize=10)
+    f.set(xscale='log', yscale='log')
+    f.tight_layout()
+    f.axes[0,0].set_ylabel(r"$|E_{\theta}-E_{gs}|/|E_{gs}|$")
+    f.axes[0,0].set_xticks(unique_N)
+    f.axes[0,0].set_xticklabels(unique_N)
+elif isinstance(args.J2, list):
+    f = sns.relplot(
+        data=df, kind='line',
+        x='N', y='rel_error',
+        col='J2', col_order=sorted(args.J2),
+        hue='samples', style='ansatz',
+        markers=True, dashes=False,
+        palette='tab10', markersize=10)
+    f.set(xscale='log', yscale='log')
+    f.tight_layout()
+    f.axes[0,0].set_ylabel(r"$|E_{\theta}-E_{gs}|/|E_{gs}|$")
+    for ax in f.axes.ravel():
+        ax.set_xticks(unique_N)
+        ax.set_xticklabels(unique_N)
 # sns.move_legend(f, "upper right", bbox_to_anchor=(0.6, 0.9))
 if args.title:
     f.set_title(args.title)
