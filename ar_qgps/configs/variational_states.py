06f5a276e1024b1249b174ca4b3cd2e20260c599
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2022-05-17 15:10:25 +0100

    Add chunk_size parameter

--- ar_qgps/configs/variational_states.py
+++ ar_qgps/configs/variational_states.py
@@ -1,4 +1,5 @@
 from ml_collections import ConfigDict
+from ml_collections.config_dict import placeholder
 
 
 def get_MCState_config(parent : ConfigDict) -> ConfigDict:
@@ -6,6 +7,7 @@ def get_MCState_config(parent : ConfigDict) -> ConfigDict:
     config.n_samples = 100
     if parent.get_ref('sampler_name') != 'ARDirectSampler':
         config.n_discard_per_chain = config.get_ref('n_samples') // 10
+    config.chunk_size = placeholder(int)
     return config
 
 def get_ExactState_config(parent : ConfigDict) -> ConfigDict:
