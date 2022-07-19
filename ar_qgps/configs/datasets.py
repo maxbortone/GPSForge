9b94f690af48225e5add37a1d53c2bd64f318cb2
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2022-07-19 18:50:23 +0100

    Minor fix

--- ar_qgps/configs/datasets.py
+++ ar_qgps/configs/datasets.py
@@ -1,10 +1,11 @@
 from ml_collections import ConfigDict
+from ml_collections.config_dict import placeholder
 
 
 def get_H2O_config():
     config = ConfigDict()
     config.basis = 'canonical'
     config.method = 'fci'
-    config.select_largest = 100
+    config.select_largest = placeholder(int)
     config.datapath = "/tmp/qGPSKet_data"
     return config
