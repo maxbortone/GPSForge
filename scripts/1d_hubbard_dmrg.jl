b504e35c6ed2724d2587a6d25287dbcf5fa78c55
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2023-02-08 14:33:59 +0000

    Add DMRG script

---
+++ scripts/1d_hubbard_dmrg.jl
@@ -0,0 +1,91 @@
+using ITensors
+
+#
+# DMRG calculation of the 1D Hubbard model
+# ground state wavefunction, and spin densities
+#
+
+let
+    N = 32
+    Npart = 32
+    t = 1.0
+    U = 8.0
+    
+    sites = siteinds("Electron", N; conserve_qns=true)
+    
+    ampo = OpSum()
+    for b in 1:(N-1)
+        ampo += -t, "Cdagup", b, "Cup", b + 1
+        ampo += -t, "Cdagup", b + 1, "Cup", b
+        ampo += -t, "Cdagdn", b, "Cdn", b + 1
+        ampo += -t, "Cdagdn", b + 1, "Cdn", b
+    end
+    if N % 4 == 0
+        t *= -1
+    end
+    ampo += -t, "Cdagup", N, "Cup", 1
+    ampo += -t, "Cdagup", 1, "Cup", N
+    ampo += -t, "Cdagdn", N, "Cdn", 1
+    ampo += -t, "Cdagdn", 1, "Cdn", N
+    for i in 1:N
+        ampo += U, "Nupdn", i
+    end
+    H = MPO(ampo, sites)
+    
+    nsweeps = 32
+    maxdim = 1600 # [50, 100, 200, 400, 800, 800, 800, 800, 800, 800]
+    cutoff = [1E-12]
+    
+    state = ["Emp" for n in 1:N]
+    p = Npart
+    for i in N:-1:1
+        if p > i
+            println("Doubly occupying site $i")
+            state[i] = "UpDn"
+            p -= 2
+        elseif p > 0
+            println("Singly occupying site $i")
+            state[i] = (isodd(i) ? "Up" : "Dn")
+            p -= 1
+        end
+    end
+    # Initialize wavefunction to be bond 
+    # dimension 10 random MPS with number
+    # of particles the same as `state`
+    psi0 = randomMPS(sites, state, 10)
+    
+    # Check total number of particles:
+    @show flux(psi0)
+    
+    # Start DMRG calculation:
+    energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff)
+    
+    upd = fill(0.0, N)
+    dnd = fill(0.0, N)
+    for j in 1:N
+        orthogonalize!(psi, j)
+        psidag_j = dag(prime(psi[j], "Site"))
+        upd[j] = scalar(psidag_j * op(sites, "Nup", j) * psi[j])
+        dnd[j] = scalar(psidag_j * op(sites, "Ndn", j) * psi[j])
+    end
+    
+    println("Up Density:")
+    for j in 1:N
+        println("$j $(upd[j])")
+    end
+    println()
+    
+    println("Dn Density:")
+    for j in 1:N
+        println("$j $(dnd[j])")
+    end
+    println()
+    
+    println("Total Density:")
+    for j in 1:N
+        println("$j $(upd[j]+dnd[j])")
+    end
+    println()
+    
+    println("\nGround State Energy = $energy")
+end
