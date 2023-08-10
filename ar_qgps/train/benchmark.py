4357f3b8f6dfff6a141a856f7aa4ac6dc0729136
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2023-08-10 18:06:00 +0100

    Minor updates

--- ar_qgps/train/benchmark.py
+++ ar_qgps/train/benchmark.py
@@ -41,7 +41,7 @@ def benchmark(config: ml_collections.ConfigDict, workdir: str):
     g = ha.graph if hasattr(ha, 'graph') else None
 
     # Ansatz model
-    ma = get_model(config, hi, g, ha)
+    ma = get_model(config, hi, g, ha, workdir)
 
     # Sampler
     sa = get_sampler(config, hi, g)
@@ -55,7 +55,7 @@ def benchmark(config: ml_collections.ConfigDict, workdir: str):
     # Driver
     if config.optimizer_name == 'minSR':
         solver = lambda A, b: jnp.linalg.lstsq(A, b, rcond=config.optimizer.rcond)[0]
-        vmc = qk.driver.minSRVMC(ha, op, variational_state=vs, mode=config.optimizer.mode, minSR_solver=solver, diag_shift=config.optimizer.diag_shift)
+        vmc = qk.driver.minSRVMC(ha, op, variational_state=vs, mode=config.optimizer.mode, minSR_solver=solver)
     else:
         vmc = nk.driver.VMC(ha, op, variational_state=vs, preconditioner=sr)
 
