918b11e3a78a0dc9bc43e622bd60b399952b27a1
Massimo Bortone <massimo.bortone@kcl.ac.uk>
2023-08-24 11:17:42 +0100

    Add support for molecular systems with frozen core

--- scripts/molecular_ccsd.py
+++ scripts/molecular_ccsd.py
@@ -7,13 +7,13 @@ from absl import flags
 from absl import logging
 from pyscf import gto, scf, cc
 from ml_collections import config_flags
-from ar_qgps.configs import systems
 from ar_qgps.configs.common import resolve
 
 
 FLAGS = flags.FLAGS
 
-method = flags.DEFINE_string('method', 'ccsd', 'CC method used to compute the energy')
+method = flags.DEFINE_string('method', 'CCSD', 'CC method used to compute the energy. Choose between: CCSD and CCSD(T)')
+restricted = flags.DEFINE_bool('restricted', True, 'Flag to choose between a calculation with restricted or unrestricted spin orbitals')
 
 _CONFIG = config_flags.DEFINE_config_file(
     'config',
@@ -32,15 +32,32 @@ def main(argv):
     logging.info('Config: %s', config)
 
     # Setup molecular system
+    if config.system.get('n_elec', None):
+        n_elec = config.system.get('n_elec')
+        spin = n_elec[0]-n_elec[1]
+    else:
+        spin = 0
     mol = gto.M(
         atom=config.system.molecule,
         basis=config.system.basis_set,
         symmetry=config.system.symmetry,
-        unit=config.system.unit
+        unit=config.system.unit,
+        spin=spin
     )
     logging.info(f"Nuclear energy {mol.energy_nuc()}")
-    mf = scf.HF(mol).run()
-    mycc = cc.CCSD(mf).run()
+    if restricted.value:
+        mf = scf.RHF(mol)
+    else:
+        mf = scf.UHF(mol)
+    if config.system.get('sfx2c1e', None):
+        mf = mf.sfx2c1e()
+    mf.run()
+    frozen = config.system.get('frozen_electrons', 0)//2
+    if restricted.value:
+        mycc = cc.RCCSD(mf, frozen=frozen)
+    else:
+        mycc = cc.UCCSD(mf, frozen=frozen)
+    mycc.run()
     logging.info(f"CCSD total energy {mycc.e_tot}")
     if method.value == "CCSD(T)":
         et = mycc.ccsd_t()
